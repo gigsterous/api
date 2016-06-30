fd4df4815cd21d548b4a607aa79fb97913fa67a8
Martin Myslík <m.myslik@gmail.com>
2016-06-30 10:45:02 +0200

    Converted project to WAR packaging and added comments to pom

--- src/main/java/com/gigsterous/api/App.java
+++ src/main/java/com/gigsterous/api/App.java
@@ -2,9 +2,10 @@ package com.gigsterous.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
