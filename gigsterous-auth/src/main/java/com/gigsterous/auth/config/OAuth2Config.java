3ad587065e5ccd5821a8245281d9fbf710b3b380
Martin Myslík <martin@berrycloud.cz>
2016-08-04 11:24:41 +0200

    # changed package structure in auth server

---
+++ gigsterous-auth/src/main/java/com/gigsterous/auth/config/OAuth2Config.java
@@ -0,0 +1,27 @@
+package com.gigsterous.auth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager);
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("foo").secret("foosecret")
+				.authorizedGrantTypes("authorization_code", "refresh_token", "password").scopes("openid");
+	}
+}
