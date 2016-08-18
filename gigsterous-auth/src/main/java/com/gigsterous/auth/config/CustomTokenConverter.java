df463bc2031243d961c39f35094d6c732b7d1698
Martin Myslík <martin@berrycloud.cz>
2016-08-18 09:54:22 +0200

    #10 passing user details to resource server

--- gigsterous-auth/src/main/java/com/gigsterous/auth/config/CustomTokenConverter.java
+++ gigsterous-auth/src/main/java/com/gigsterous/auth/config/CustomTokenConverter.java
@@ -10,13 +10,18 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 
 import com.gigsterous.auth.domain.User;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Custom JWT token in which additional information can be stored. For example add the user's ID to each token.
  */
+@Slf4j
 public class CustomTokenConverter extends JwtAccessTokenConverter {
 
 	@Override
 	public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+		log.debug("Enhancing access token: {}", accessToken.toString());
+		
 		Map<String, Object> info = new LinkedHashMap<>(accessToken.getAdditionalInformation());
 
 		info.put("user_id", ((User) authentication.getPrincipal()).getId());
--- gigsterous-auth/src/main/java/com/gigsterous/auth/controller/UserController.java
+++ gigsterous-auth/src/main/java/com/gigsterous/auth/controller/UserController.java
@@ -2,15 +2,29 @@ package com.gigsterous.auth.controller;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gigsterous.auth.domain.User;
+import com.gigsterous.auth.repository.UserRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class UserController {
 	
+	@Autowired
+	private UserRepository userRepo;
+	
 	@RequestMapping("/user")
-	public Principal user(Principal user) {
-		return user;
+	public User user(Principal principal) {
+		log.debug("Getting user based on principal: {}", principal.toString());
+		
+		
+		
+		return userRepo.findOneByUsername(principal.getName());
 	}
 
 }
