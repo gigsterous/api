b1d0ba34bedda6b0781b26ad70860e58f2ce29ab
Martin Myslik <m.myslik@gmail.com>
2016-10-01 13:59:51 +0200

    #22 added password encryption using BCrypt

--- gigsterous-auth/src/main/java/com/gigsterous/auth/config/OAuth2Config.java
+++ gigsterous-auth/src/main/java/com/gigsterous/auth/config/OAuth2Config.java
@@ -3,9 +3,12 @@ package com.gigsterous.auth.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -14,7 +17,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 @Configuration
 @EnableAuthorizationServer
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
-	
+
 	@Autowired
 	@Qualifier("userDetailsService")
 	private UserDetailsService userDetailsService;
@@ -25,6 +28,12 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 	@Value("${gigsterous.oauth.tokenTimeout:3600}")
 	private int expiration;
 
+	// password encryptor
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer configurer) throws Exception {
 		configurer.authenticationManager(authenticationManager);
@@ -33,13 +42,8 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory()
-			.withClient("gigsterous")
-			.secret("secret")
-			.accessTokenValiditySeconds(expiration)
-			.scopes("read", "write")
-			.authorizedGrantTypes("password", "refresh_token")
-			.resourceIds("resource");
+		clients.inMemory().withClient("gigsterous").secret("secret").accessTokenValiditySeconds(expiration)
+				.scopes("read", "write").authorizedGrantTypes("password", "refresh_token").resourceIds("resource");
 	}
 
 }
--- gigsterous-auth/src/main/resources/db/development/V1_1__Test_Data.sql
+++ gigsterous-auth/src/main/resources/db/development/V1_1__Test_Data.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (user_id, email, username, password, enabled) VALUES 
-	('1', 'peter@hotmail.com', 'peter', 'password', true),
-	('2', 'john@email.cz', 'john', 'password', true);
+	('1', 'peter@hotmail.com', 'peter', '$2a$10$D4OLKI6yy68crm.3imC9X.P2xqKHs5TloWUcr6z5XdOqnTrAK84ri', true),
+	('2', 'john@email.cz', 'john', '$2a$10$D4OLKI6yy68crm.3imC9X.P2xqKHs5TloWUcr6z5XdOqnTrAK84ri', true);
